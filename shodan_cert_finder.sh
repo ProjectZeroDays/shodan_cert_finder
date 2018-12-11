cf3644dbf0ff6f69884e7b0b4b42c9ae4ec0e56e
Aljaz Ceru <aljaz@ceru.si>
2018-12-11 23:00:37 +0100

    bash cleanup

--- shodan_cert_finder.sh
+++ shodan_cert_finder.sh
@@ -1,14 +1,17 @@
 #!/bin/bash
+RESULTS_FILE="/tmp/serial_searcher"
+
 if [ $# -eq 0 ]; then
     echo ""
     echo "please provide FQDN as an argument"
     echo ""
-    echo "example: bash shodan_serial_finder.sh www.google.com"
+    echo "example: bash shodan_serial_searcher.sh www.google.com"
     echo ""
     exit 1
 fi
-SERIAL=$(openssl s_client -connect $1:443 -servername $1 < /dev/null 2>/dev/null | openssl x509 -serial -noout -in /dev/stdin | awk -F= '{print $2}' )
+
+SERIAL=$(openssl s_client -connect "$1":443 -servername "$1" < /dev/null 2>/dev/null | openssl x509 -serial -noout -in /dev/stdin | awk -F= '{print $2}' )
 echo "SERIAL: $SERIAL "
-shodan search --fields ip_str,port,org,hostnames ssl.cert.serial:$SERIAL > /tmp/serial_searcher
-cat /tmp/serial_searcher_results
-rm /tmp/serial_searcher_results
+shodan search --fields ip_str,port,org,hostnames ssl.cert.serial:"$SERIAL" > "$RESULTS_FILE"
+cat "$RESULTS_FILE"
+rm "$RESULTS_FILE"
